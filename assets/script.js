c6abed85f4f15640877ebd24d3756acb1cbf501f
Ahmed-Ghazal55 <gazalle22@gmail.com>
2023-03-06 19:17:41 +0200

    updata card

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
   // var $filterHeadIcon = $('.filter-head-icon .bi');
 
 
+  displayCart();
+
   $showViewBtn.each(function () {
     $(this).on('click', function () {
       $overlayQuickView.toggle();
@@ -128,7 +130,7 @@ function clickFilter() {
   });
 };
 
-
+// cart page function
 
 const products = [
   {
@@ -153,9 +155,11 @@ const products = [
     price: 35,
   },
 ];
+
 const categories = [...new Set(products.map((item)=>
   {return item}))]
   let i = 0;
+
 
 document.querySelector(".product-overlay").innerHTML = categories.map((item)=>
 {
@@ -234,6 +238,10 @@ function deletCart(a){
   displayCart();
 }
 
+function selectProduct(){
+  document.querySelector('.overlay-quick-view.product-overlay').style.display = 'flex';
+}
+
 function displayCart(a){
   let j = 0;
   if(cart.length==0) {
@@ -245,7 +253,7 @@ function displayCart(a){
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, dolor?
       </p>
-      <div class="container col-lg-6 col-md-10 col-sm-12">
+      <div class="container col-lg-6 col-md-10 col-sm-12 select-add">
         <h6>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, esse!
         </h6>
@@ -257,7 +265,7 @@ function displayCart(a){
             <p class="d-flex flex-row">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti, laudantium?
             </p>
-            <button type="button" class="btn btn-select">
+            <button type='button' class='btn btn-select' onclick='selectProduct()'>
               select + add to cart
             </button>
           </div>
@@ -269,113 +277,126 @@ function displayCart(a){
     </div>`
   }
   else {
-    document.getElementById('myCart').innerHTML = cart.map((items)=>{
+    // document.getElementById('myCart').innerHTML = cart.map((items)=>{
+    //   var {imgs, name, size, price} = items;
+    //   var total = 0 ;
+    //   total += price;
+    //   return(
+        
+    //   );
+    // }).join('');
+
+    var total = 0
+    cart.map((items)=>{
       var {imgs, name, size, price} = items;
-      var total = 0 ;
-      total += price;
-      return(
-        `
-        <div class="my-carts w-100 p-1 m-1">
-          <div class="col p-1 h-100">
-            <div class="row w-100 h-100">
-              <div class="col-lg-8 col-md-8 col-sm-12">
-                <div class="container col-12 select-add">
-                  <h6 class="col-12">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, esse!
-                  </h6>
-                  <div class="row w-100 justify-content-center align-items-center">
-                    <div class="col-lg-3 col-md-3 col-sm-10 justify-content-center">
-                      <img src="assets/imgs/classic-turkish-cotton-towels_blush_lightbox_0194.jpg" alt="" class="img-fluid">
-                    </div>
-                    <div class="col-lg-9 col-md-9 col-sm-10 justify-content-center">
-                      <p class="d-flex flex-row">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti, laudantium?
-                      </p>
-                      <button type="button" class="btn btn-select">
-                        select + add to cart
-                      </button>
-                    </div>
-                  </div>
+      total += parseInt(price);
+    })
+    document.getElementById('myCart').innerHTML = `
+      <div class="my-carts w-100 p-1 m-1">
+      <div class="col p-1 h-100">
+        <div class="row w-100 h-100">
+          <div class="col-lg-8 col-md-8 col-sm-12">
+            <div class="container col-12 select-add">
+              <h6 class="col-12">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, esse!
+              </h6>
+              <div class="row w-100 justify-content-center align-items-center">
+                <div class="col-lg-3 col-md-3 col-sm-10 justify-content-center">
+                  <img src="assets/imgs/classic-turkish-cotton-towels_blush_lightbox_0194.jpg" alt="" class="img-fluid">
                 </div>
+                <div class="col-lg-9 col-md-9 col-sm-10 justify-content-center">
+                  <p class="d-flex flex-row">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti, laudantium?
+                  </p>
+                  <button type="button" class="btn btn-select" onclick='selectProduct()'>
+                    select + add to cart
+                  </button>
+                </div>
+              </div>
+            </div>`+ cart.map((items)=>{
+              var {imgs, name, size, price} = items;
+              
+              return(`
                 <div class="w-100 p-3 d-flex flex-row justify-content-between border-bottom">
                   <h2 class="text-muted text-capitalize">cart</h2>
                   <h5 class="text-muted">( ${cart.length} Items )</h5>
-                </div>
-                <div class="cart-item p-2">
-                  <div class="row h-100">
-                    <div class="col-3 p-1 d-flex flex-column align-items-center h-100">
-                      <img src="assets/imgs/${imgs[0]}" alt="" class="img-fluid h-75">
-                    </div>
-                    <div class="p-2 col-6 d-flex flex-column justify-content-center align-items-start text-muted">
-                      <h5>${name}</h5>
-                      <br>
-                      <h6 class="small">${size}</h6>
-                    </div>
-                    <div class="col d-flex flex-column text-end justify-content-between">` +
-                      "<button class='btn-close ms-auto' onclick='deletCart("+(j++)+")'> </button>" +
-                      `<h6 class="small">
-                        $ ${price}
+                  </div>
+                  <div class="cart-item p-2">
+                    <div class="row h-100">
+                      <div class="col-3 p-1 d-flex flex-column align-items-center h-100">
+                        <img src="assets/imgs/${imgs[0]}" alt="" class="img-fluid h-75">
+                      </div>
+                      <div class="p-2 col-6 d-flex flex-column justify-content-center align-items-start text-muted">
+                        <h5>${name}</h5>
                         <br>
-                        <span class="text-capitalize">final sale</span>
-                      </h6>
+                        <h6 class="small">${size}</h6>
+                      </div>
+                      <div class="col d-flex flex-column text-end justify-content-between">` +
+                        "<button class='btn-close ms-auto' onclick='deletCart("+(j++)+")'> </button>" +
+                        `<h6 class="small">
+                          $ ${price}
+                          <br>
+                          <span class="text-capitalize">final sale</span>
+                        </h6>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div class="p-3 col-lg-4 col-md-4 col-sm-12 micro-checkout">  
-                <div class="row">
-                  <div class="col-12">
-                    <div class="summary">
-                      <h2>
-                        <p>summary</p>
-                      </h2>
-                      <h6>
-                        <p>subtotal</p>
-                        <p>$ 7</p>
-                      </h6>
-                      <h6>
-                        <p>shipping</p>
-                        <p>calculated at next step</p>
-                      </h6>
-                      <h6>
-                        <p>tax</p>
-                        <p>calculated at next step</p>
-                      </h6>
-                    </div>
-                    <div class="total">
-                      <p>total</p>
-                      <p>usd <strong>$ ${total}</strong></p>
-                    </div>
-                    <div class="btns">
-                      <a class="btn" href="checkout.html">
-                        checkout
-                      </a>
-                      <a class="btn">
-                        express checkout
-                      </a>
-                    </div>
-                    <div class="free text-capitalize p-2 bg-body-secondary">
-                      <h5 class="text-center p-2">
-                        Free Carbon Neutral Shipping + Returns
-                      </h5>
-                      <p class="text-start text-wrap">
-                        We're Climate Neutral certified, which means we offset our emissions on everything from product to shipping. Learn More
-                        <br>
-                        If you are unsatisfied with your purchase for any reason, we are happy to accept returns within 60 days (excludes final sale items).
-                        <br>
-                        White glove delivery fees, expedited and oversize shipping fees cannot be refunded.
-                        <br>
-                        Select furniture items are subject to restocking fees.
-                      </p>
-                    </div>
+                  `
+              )
+            }).join('') + `
+            </div>
+            <div class="p-3 col-lg-4 col-md-4 col-sm-12 micro-checkout">  
+              <div class="row">
+                <div class="col-12">
+                  <div class="summary">
+                    <h2>
+                      <p>summary</p>
+                    </h2>
+                    <h6>
+                      <p>subtotal</p>
+                      <p>$ ${total}</p>
+                    </h6>
+                    <h6>
+                      <p>shipping</p>
+                      <p>calculated at next step</p>
+                    </h6>
+                    <h6>
+                      <p>tax</p>
+                      <p>calculated at next step</p>
+                    </h6>
+                  </div>
+                  <div class="total">
+                    <p>total</p>
+                    <p>usd <strong>$ ${total}</strong></p>
+                  </div>
+                  <div class="btns">
+                    <a class="btn" href="checkout.html">
+                      checkout
+                    </a>
+                    <a class="btn">
+                      express checkout
+                    </a>
+                  </div>
+                  <div class="free text-capitalize p-2 bg-body-secondary">
+                    <h5 class="text-center p-2">
+                      Free Carbon Neutral Shipping + Returns
+                    </h5>
+                    <p class="text-start text-wrap">
+                      We're Climate Neutral certified, which means we offset our emissions on everything from product to shipping. Learn More
+                      <br>
+                      If you are unsatisfied with your purchase for any reason, we are happy to accept returns within 60 days (excludes final sale items).
+                      <br>
+                      White glove delivery fees, expedited and oversize shipping fees cannot be refunded.
+                      <br>
+                      Select furniture items are subject to restocking fees.
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>`
-      )
-    }).join('');
+        </div>
+      </div>`
   }
 }
 
